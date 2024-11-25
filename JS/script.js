d85c80d14149169e1468c5e3ff5d1cfcad6f7d15
Pranjal-githubX <pranjaltomar132@gmail.com>
2024-11-25 17:52:12 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -102,7 +102,7 @@ const playlist = [
     }, {
         name: 'Parda Parda',
         artist: 'Pritam | Sunidhi Chauhan | Rana Mazumder',
-        thumbnail: './More/mumbai.jpeg',
+        thumbnail: './More/Mumbai.jpeg',
         audioPath: 'Audio/parda.mp3'
     }, {
         name: 'Pee Loon',
